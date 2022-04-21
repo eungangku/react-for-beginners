fe94725f760a34926afb97393dc7f7cb89c30b35
eungangku <eungangku@gmail.com>
2022-04-22 00:02:41 +0900

    dark theme suppport

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import styles from "./App.module.css";
 import Movie from "./routes/MovieList";
 import Detail from "./routes/MovieDetail";
 import RandomArt from "./routes/RandomArt";
-import PomodoroFocus from "./routes/PomodoroTimer";
+import PomodoroTimer from "./routes/PomodoroTimer";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function Home() {
   return (
@@ -20,33 +23,48 @@ function Home() {
 }
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/detail/:id">
-            <ScrollToTop />
-            <Detail />
-          </Route>
-          <Route path="/randomart">
-            <ScrollToTop />
-            <RandomArt />
-          </Route>
-          <Route path="/movie">
-            <Movie />
-          </Route>
-          <Route path="/pomodoro/rest">
-            <PomodoroFocus type={"rest"} time={60 * 6} />
-          </Route>
-          <Route path="/pomodoro">
-            <PomodoroFocus type={"focus"} time={60 * 30} />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/detail/:id">
+              <ScrollToTop />
+              <Detail />
+            </Route>
+            <Route path="/randomart">
+              <ScrollToTop />
+              <RandomArt />
+            </Route>
+            <Route path="/movie">
+              <Movie />
+            </Route>
+            <Route path="/pomodoro/rest">
+              <PomodoroTimer type={"rest"} time={60 * 6} />
+            </Route>
+            <Route path="/pomodoro">
+              <PomodoroTimer type={"focus"} time={60 * 30} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
